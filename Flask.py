c9799753c734ca6b1ae242e65da5eb547fd63e06
DonielMoins <theshark556@gmail.com>
2021-04-05 04:02:30 -0400

    Failed multiprocessing, Over engineered code

--- Flask.py
+++ Flask.py
@@ -1,135 +1,55 @@
+# import all the py file because we need a reference to the vars 
+# when using from ? import var, it makes a copy of the var's value instead
+# We need a reference so that we can access this var from wherever, in case we need it.
 from flask import Flask, request, render_template, redirect, url_for
+import multiprocessing.managers
+
+
 from markupsafe import Markup
-from flask_table import create_table, Col, Table
 from functools import cache
-import mariadb
+from helper.db_helper import DB_INFO, MakeConnectionPool, GetConnection
+from config import db_settings
+from helper.table_helper import build_table, get_column_names, isTable
+from multiprocessing.managers import DictProxy
+from dataTypes.multiprocessing import DataCouple
 
-
-# SET DB INFO HERE
-# Unecessary Immutable list but it sure makes my code look cooler
-# Sorry ive been writing this for about 7 hours now and im almost done
-class DB_INFO:
-    def __init__(self):
-        self.host      =    "localhost"
-        self.user      =    "anonymous"
-        self.passwd    =    ""
-        self.port      =    3306
-        self.db_name   =    "test"
-        self.pool_name =    "FlaskDatabaseviewer"
-        self.pool_size =    10
-        # self.database  =    "DATABASE_NAME"
-
-global db_Info
-global db_Conn_Pool
-
-def GetConnection(connection_pool):
-    try:
-        if type(db_Conn_Pool) != None:  
-            return connection_pool.get_connection()
-        else:
-            connection_pool = MakeConnectionPool(db_Info)
-            return GetConnection(connection_pool)
-    except mariadb.PoolError as e:
-        print(f"Error opening connection from pool: {e}")
-
-def MakeConnectionPool(db_info):
-        try:
-            db_conn_pool =   mariadb.ConnectionPool(
-                            user=db_info.user,
-                            password=db_info.passwd,
-                            host=db_info.host,
-                            port=db_info.port,
-                            pool_name=db_info.pool_name,
-                            pool_size=db_info.pool_size
-                        )
-            return db_conn_pool
-        except mariadb.Error as e:
-            return None
-
-db_Info = DB_INFO()
-db_Conn_Pool = MakeConnectionPool(db_Info)
-
-# def CheckPool():
-#     if type(db_Conn_Pool) is None:
-#         db_Conn_Pool = MakeConnectionPool(db_Info)
+from dataManager import startDataManager, ConnectionPool
 
 
 # Setup Flask App
-app = Flask(__name__)
+app             = Flask(__name__)
+memoryManager   = multiprocessing.Manager()
 
-# Declare your table
-def build_table(cursor, data):
-    
-    # classes = ['table', 'table-bordered']
-    # no_items    = 'There are no results for this query'
-    if type(cursor) != None:
-        db_columns = cursor.description
-        TableMeta = create_table()
-        cols = []
-        for db_column in db_columns:
-            TableMeta.add_column(str(db_column[0]), Col(str(db_column[0])))
-            cols.append(str(db_column[0]))
-        tableItems = []
-        for rowIndex in range(len(data)):
-            row = data[rowIndex]
-            rowDict = dict()
+db_Info = None
+db_Conn_Pool = None
+
+# class Flask:
+#     # def __init__(self, namespaces, accessEvents):
+#     #     self.namespaces = namespaces
+#     #     self.accessEvents = accessEvents
+#     #     for namespace in namespaces:
+#     #         if namespace == "db":
+#     #             self += namespace
             
-            for colIndex in range(len(cols)):
-                rowDict[cols[colIndex]] = row[colIndex]
-            tableItems.append(rowDict)
-            
-    
-    
+#     def __init__(self, DataCoupleDict: DictProxy):
+#         for value in DataCoupleDict.values():
+#             print(value.namespace.values())
         
+#         # for key, dataCouple in iter(DataCoupleDict):
             
-    # result = []
-    # for value in data:
-    #     tmp = {}
-    #     for (index,column) in enumerate(value):
-    #         tmp[columns[index][0]] = column
-
-    # table = TableMeta()    
-    
-    table = TableMeta(tableItems)
-    table.classes = ['table', 'table-bordered']
-
-    return table
-                
-    
-
-def get_column_names(cursor):
-    return cursor.column_names
-
-# Get some objects
-
-# class Item(object):
-#     def __init__(self, name, description):
-#         self.name = name
-#         self.description = description
-
-
-
-
-# Or, equivalently, some dicts
-# items = [dict(name='Name1', description='Description1'),
-#          dict(name='Name2', description='Description2'),
-#          dict(name='Name3', description='Description3')]
-
-# Initialize a Table with all the items then add it to element list
-
-# items = [Item('Name1', 'Description1'),
-#          Item('Name2', 'Description2'),
-#          Item('Name3', 'Description3')]
-# table = ItemTable(items)
-
+#         #     if str(key) is "db":
+#         #         dc.namespace["db_Info"], self.db_Info = DB_INFO(db_settings, "   ", "TEST")
+#         #         dc.namespace["db_Conn_Pool"], self.db_Conn_Pool = MakeConnectionPool(self.db_Info)
+#         super().__init__()
+   
 WebElements = []
-
 # Pretty sure we can do this since we dont change much with the webpage 
 # TODO: Test it till it breaks
-def StupidConcatElements():
+@cache
+def ConcatElements():
     ConcatResult = ""
     for Element in WebElements:
-        if isinstance(Element, Table):
+        if isTable(Element):
             ConcatResult += Element.__html__()
         else:
             try:
@@ -138,9 +58,6 @@ def StupidConcatElements():
                 ConcatResult += "ELEMENT ERROR"
     WebElements.clear()
     return Markup(ConcatResult)
-
-# Add function to 
-app.jinja_env.globals.update(StupidConcatElements=StupidConcatElements())
 
 # Fallback page for incorrect addresses
 @cache
@@ -152,58 +69,60 @@ def fallbackRedirect(u_path):
 # Landing Page
 @cache
 @app.route('/query', methods = ['GET'])
-def queryPage(Error=None):
-    return render_template("base.html", Error=Error)
-
-
+def queryPage(E=None):
+    if ConcatElements() not in app.jinja_env.globals:
+        app.jinja_env.globals.update(Elements=ConcatElements())
+    return render_template("base.html", Error=E)
 # TODO: Save Queries in folder
 # TODO: User specific Query folders?
 # TODO: Only allow viewing of data
-
 # Check else statement for info on GET method
+
 @app.route('/data', methods = ['POST', 'GET'])
 def tablePage():
-        if request.method == 'POST':
-            # listDicRec(0, request.form)
-            
-            use_Old_Data = True if "use_Old_Data" in request.form else False
-            
-            if "requested_Query" in request.form and str(request.form["requested_Query"]) != None and str(request.form["requested_Query"]).strip() != "":     
-                requested_Query = request.form["requested_Query"]
-
-                try:
-                    curr = GetConnection(db_Conn_Pool).cursor()
-                    curr.execute(f"USE {db_Info.db_name};")
-                    curr.execute(requested_Query)
-
-                    data = curr.fetchall()
-
-                    table = build_table(curr, data)
-                    WebElements.append(table)
-                    app.jinja_env.globals.update(Elements=StupidConcatElements())
-                    curr.close()
-                except mariadb.Error as e:
-                    return queryPage(e)
-                return render_template("tableDisplay.html")
-        # Added GET so that users dont hit a "Method Not Allowed Page" somehow
-        return redirect("/query", code=302)
-
-# Debug function
-# def listDicRec(lev, data):
-#     print("\n") 
-#     if data.values() is not None:
-#         for i in data:
-            
-#             if isinstance(i, list):
-#                 print((lev*"\t") + i+":",),
-#                 listDicRec(lev+1, i)
-#             else:
-#                 print((lev*"\t"),f"{i}:\t{data[i]}")
-#     print("\n")           
-#     print("DONE")
-            
+    if request.method == 'POST':
+        # listDicRec(0, request.form)
+        # Im thinking this isnt even useful
+        # use_Old_Data = True if "use_Old_Data" in request.form else False
+        if "requested_Query" in request.form and str(request.form["requested_Query"]) != None and str(request.form["requested_Query"]).strip() != "":     
+            requested_Query = request.form["requested_Query"]
+            try:
+                # 
+                curr = GetConnection(db_Info, db_Conn_Pool).cursor()
+                curr.execute(f"USE {db_Info.db_name};")
+                curr.execute(requested_Query)
+                data = curr.fetchall()
+                table = build_table(curr, data)
+                WebElements.append(table)
+                app.jinja_env.globals.update(Elements=ConcatElements())
+                curr.close()
+            except Exception as e:
+                return queryPage(e)
+            return render_template("tableDisplay.html")
+    # Added GET so that users dont hit a "Method Not Allowed Page" somehow
+    return redirect("/query", code=302)
     
+# def startFlask(self, data):
+#     global db_Info
+#     global db_Conn_Pool
+#     globals().update(db_Info, DB_INFO(db_settings)
+#     globals()["db"].db_Conn_Pool, db_Conn_Pool = MakeConnectionPool(db_Info)
+    
+data = startDataManager(memoryManager)
+DataCouplesProxy = dict([dc for dc in data.items()])
 
+DataCouples = [DataCouplesProxy[key].get() for key in DataCouplesProxy.keys()]
 
-if __name__ == '__main__':
-    app.run(debug=True)
+namespaceDict = list()
+
+for DataCouple in DataCouples:
+    namespaceDict.append(DataCouple.namespace._getvalue().__dict__)
+namespaceDict = namespaceDict.pop(namespaceDict.__len__() - 1)
+for varName in namespaceDict:    
+    if db_Info == None:
+        connPool = MakeConnectionPool(namespaceDict["db_Info"].get())
+        memoryManager.Value(type(ConnectionPool), connPool)
+        namespaceDict[varName].set() 
+    globals()[varName] = namespaceDict[varName]
+
+print("all ready")
---
+++ helper/db_helper.py
@@ -0,0 +1,79 @@
+import config
+import mariadb
+from mariadb import ConnectionPool
+from dataTypes.db import DB_INFO
+
+# from utility.utils.
+
+# Gives you a fresh connection
+            
+
+def GetConnection(db_Info: DB_INFO, connection_pool: ConnectionPool):
+    try:
+        # Does pool respond/exist?
+        if connection_pool:
+            return connection_pool.get_connection()
+        else:
+            # Creating pool
+            connection_pool = MakeConnectionPool(db_Info)
+            print("Creating Connection Pool!")
+            if connection_pool:
+                return GetConnection(db_Info, connection_pool)
+            else:
+                try:
+                    # Let it throw the error
+                    GetConnection(db_Info, connection_pool)
+                except Exception as e:
+                    if config.verbose:
+                        print("Connection:", e)
+                    else:
+                        print(
+                            "Connection: Cannot create connection! Check if pool is dead")
+    except mariadb.PoolError as e:
+        # Check if DB has died since last query.
+        if issubclass(e, AttributeError) and "cursor" in str(e):
+            print("Connection to MariaDB service dead, resetting pool.")
+            
+            
+            connection_pool = MakeConnectionPool(db_Info)
+            if connection_pool:
+                conn = GetConnection(db_Info, connection_pool)
+                print("Checking if connection is pingable")
+                if conn.ping():
+                    return conn
+            else:
+                print()
+
+        print(f"Error opening connection from pool: {e}")
+    except Exception as e:
+        print(e)
+
+
+def MakeConnectionPool(db_info: DB_INFO) -> mariadb.ConnectionPool:
+    try:
+        db_conn_pool = mariadb.ConnectionPool(
+            user=db_info.user,
+            password=db_info.passwd,
+            host=db_info.host,
+            port=db_info.port,
+            pool_name=db_info.pool_name,
+            pool_size=db_info.pool_size
+        )
+        return db_conn_pool
+    except mariadb.OperationalError as e:
+        if "Access denied" in str(e):
+            raise mariadb.Error()
+    except mariadb.Error as e:
+        print(f"Making Connection Pool failed with error: \n{e}\n type: {type(e)}")
+
+# Unused
+def checkConn(self):
+    sq = "SELECT NOW()"
+    try:
+        self.cur.execute(sq)
+    except mariadb.Error as e:
+        if e.errno == 2006:
+            return self.connect()
+        else:
+            print("No connection with database.")
+            return False

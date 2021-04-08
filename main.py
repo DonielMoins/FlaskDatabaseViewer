c7062ed2c9f9773cd267ffdde537f4992d5a993f
DonielMoins <theshark556@gmail.com>
2021-04-08 03:05:28 -0400

    I HATE CSS, but it works now. Any item dropped in dir of python file or any sub dirs will be added to page.

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 # We need a reference so that we can access this var from wherever, in case we need it.
 from helper.db_helper import DB_INFO, MakeConnectionPool, GetConnection
 from helper.table_helper import build_table, isTable
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from markupsafe import Markup
-from config import db_settings, debug, sqlSuffix
+from config import db_settings, sqlSuffix
+from config import debug as debugFlask
 from os import path, walk, getcwd
 
 
@@ -47,6 +48,8 @@ def fallbackRedirect(u_path):
 
 @app.route('/query', methods=['GET'])
 def queryPage(error=""):
+    if SQLfilesMarkup(dirDict) not in app.jinja_env.globals:
+        app.jinja_env.globals.update(sqlFiles=SQLfilesMarkup(dirDict))
     if ConcatElements() not in app.jinja_env.globals:
         app.jinja_env.globals.update(Elements=ConcatElements())
     return render_template("base.html", Error=error)
@@ -58,19 +61,22 @@ def queryPage(error=""):
 
 @app.route('/data', methods=['POST', 'GET'])
 def tablePage():
+    app.jinja_env.globals.update(sqlFiles=SQLfilesMarkup(dirDict))
     if request.method == 'POST':
         # listDicRec(0, request.form)
         # Im thinking this isnt even useful
         # use_Old_Data = True if "use_Old_Data" in request.form else False
-        if "requested_Query" in request.form and \
-             str(request.form["requested_Query"]) is not None and \
-                str(request.form["requested_Query"]).strip() != "":
-            requested_Query = request.form["requested_Query"]
+        if "sqlFile" in request.form and \
+             str(request.form["sqlFile"]) is not None and \
+                str(request.form["sqlFile"]).strip() != "":
+            sqlFile = request.form["sqlFile"]
             try:
                 # 
                 curr = GetConnection(db_Info, db_Conn_Pool).cursor()
                 curr.execute(f"USE {db_Info.db_name};")
-                curr.execute(requested_Query)
+                f = open(sqlFile)
+                for line in f.readlines():
+                    curr.execute(line)
                 data = curr.fetchall()
                 table = build_table(curr, data)
                 WebElements.append(table)
@@ -82,7 +88,13 @@ def tablePage():
     # Added GET so that users dont hit a "Method Not Allowed Page" somehow
     return redirect("/query", code=302)
 
-
+def SQLfilesMarkup(dirDict):
+    result = "<form action = \'"+ url_for('tablePage') + "\' method = \'post\'>\n"
+    for key in dirDict.keys():
+        result += f"<button id=\'{dirDict.get(key)}\' name=\'sqlFile\' value=\'{dirDict.get(key)}\' type = \'submit\' class =\'mdl-cell mdl-cell--3-col mdl-button resize mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent material-icons\'>" + key + "</button>\n"
+        result += f"<div class='mdl-tooltip mdl-tooltip--large' for='{dirDict.get(key)}' style='overflow-wrap: anywhere;'>File: {dirDict.get(key)} </div>\n"
+    result += "</form>"
+    return Markup(result)
 dirDict = {}
 
 # Checks every dir from location specified for file
@@ -124,5 +136,5 @@ if __name__ == "__main__":
 
     # run.remote()
     
-    app.run(debug=debug)
+    app.run(debug=debugFlask)
 
